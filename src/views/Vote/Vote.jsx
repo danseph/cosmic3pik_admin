23b3c1d00d7ebab7090e67712e3bff90ebbaae59
neural-dev <hoi@neural.com>
2018-11-28 21:43:12 +0900

    wefewf

--- src/views/Vote/Vote.jsx
+++ src/views/Vote/Vote.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 import cp from '../../cp';
 
 class VoteList extends Component {
-	//wefewfewfewf
+	// wefewfewfewfewfewfewfewfewfewfewfewf
 	constructor(props) {
 		super(props);
 		this.state = {
