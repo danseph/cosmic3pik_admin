824677189bef1309887f1dddab2315db878dc10d
HOI <neuralbc01@gmail.com>
2018-11-28 22:09:20 +0900

    ff\

--- src/views/Vote/Vote.jsx
+++ src/views/Vote/Vote.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 import cp from '../../cp';
 
 class VoteList extends Component {
-	// testeeeeeff
+	// testeeeeeffee
 	constructor(props) {
 		super(props);
 		this.state = {
