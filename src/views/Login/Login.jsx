da292a09c0cd8be4592be802843c01ae55fc3322
Seonghuichoe <heeday0410@gmail.com>
2019-02-20 16:18:54 +0900

    Add auto login feature

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,98 +1,106 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Button from "components/CustomButton/CustomButton.jsx";
+import Checkbox from 'components/CustomCheckbox/CustomCheckbox';
 import cp from '../../cp';
 import './login.scss';
 
 export class LoginComp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userId: '',
-            passwd: '' ,
-						jump : '' ,
-        };
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+			userId: '',
+			passwd: '' ,
+			auto: false,
+		};
+	}
 
-		componentDidMount() {
-			this._input.focus();
-		}
+	componentDidMount() {
+		this._input.focus();
+	}
 
-    login() {
-			if (this.state.userId === '' || this.state.passwd === '') {
-				return alert('이메일 혹은 비밀번호를 입력해주세요');
+	login() {
+		if (this.state.userId === '' || this.state.passwd === '') {
+			return alert('이메일 혹은 비밀번호를 입력해주세요');
+		}
+		axios.post(cp.server_ip+'/api/login', {
+			id: this.state.userId,
+			pass: this.state.passwd,
+			auto: this.state.auto,
+			isAdmin: true,
+		}).then(res => {
+			if (!res.data.err && res.data.token) {
+				window.localStorage['nu_id'] = res.data.token.userId;
+				window.localStorage['nu_token'] = res.data.token.token;
+				window.location.href='/';
+				return false;
 			}
-			axios.post(cp.server_ip+'/api/login', {
-				id: this.state.userId,
-				pass: this.state.passwd,
-				isAdmin: true,
-			}).then(res => {
-				if (!res.data.err && res.data.token) {
-					window.localStorage['nu_id'] = res.data.token.userId;
-					window.localStorage['nu_token'] = res.data.token.token;
-					window.location.href='/';
-					return false;
-				}
-				// not join or invaild password
-				if (res.data.errStatus === 1 || res.data.errStatus === 2) {
-					alert('정보를 확인해주세요.');
-				} else if(res.data.errStatus === 25) {
-					alert('관리자가 아닙니다.');
-				} else {
-					alert('관리자에게 문의하세요.');
-				}
-			}).catch(err => { console.log(err); });
-		}
+			// not join or invaild password
+			if (res.data.errStatus === 1 || res.data.errStatus === 2) {
+				alert('정보를 확인해주세요.');
+			} else if(res.data.errStatus === 25) {
+				alert('관리자가 아닙니다.');
+			} else {
+				alert('관리자에게 문의하세요.');
+			}
+		}).catch(err => { console.log(err); });
+	}
 
-    render() {
-			const enterEvent = e =>  e.key === 'Enter' ? this.login() : false;
-			return (
-				<div className='login-wrap'>
-					<div className='login-box'>
-						<form className="login-form">
+	render() {
+		const enterEvent = e =>  e.key === 'Enter' ? this.login() : false;
+		return (
+			<div className='login-wrap'>
+				<div className='login-box'>
+					<form className="login-form">
 
-							<div className="login-field login-email-box">
-								<span className="login-email-text" >
-									Email
-								</span>
-								<div className="login-email-input" data-validate="Username is required">
-									<input
-										className="email-input"
-										type="text"
-										name="email"
-										value={ this.state.userId }
-										onChange= {e => this.setState({ userId: e.target.value }) }
-										onKeyPress={enterEvent}
-										autoFocus
-										ref={c => (this._input = c)}/>
-								</div>
+						<div className="login-field login-email-box">
+							<span className="login-email-text" >
+								Email
+							</span>
+							<div className="login-email-input" data-validate="Username is required">
+								<input
+									className="email-input"
+									type="text"
+									name="email"
+									value={ this.state.userId }
+									onChange= {e => this.setState({ userId: e.target.value }) }
+									onKeyPress={enterEvent}
+									autoFocus
+									ref={c => (this._input = c)}/>
 							</div>
+						</div>
 
-							<div className="login-field login-password-box">
-								<span className="login-password-text">
-									Password
-								</span>
-								<div className="login-password-input" data-validate="Password is required">
-									<input
-										className="password-input"
-										type="password"
-										name="pass"
-										value={this.state.passwd}
-										onChange={ e => this.setState({ passwd: e.target.value }) }
-										onKeyPress={enterEvent}/>
-								</div>
+						<div className="login-field login-password-box">
+							<span className="login-password-text">
+								Password
+							</span>
+							<div className="login-password-input" data-validate="Password is required">
+								<input
+									className="password-input"
+									type="password"
+									name="pass"
+									value={this.state.passwd}
+									onChange={ e => this.setState({ passwd: e.target.value }) }
+									onKeyPress={enterEvent}/>
 							</div>
+						</div>
 
-							<div className="login-field login-button-box">
-								<Button bsStyle="info" simple type="button" bsSize="small" onClick={e => this.login()}>
-									Login
-								</Button>
-							</div>
-						</form>
-					</div>
+						<div className="login-field login-button-box">
+							<Checkbox
+								changeAction={()=> {this.setState({ auto: !this.state.auto });}}
+								number="1"
+								name="autoCheck"
+								label="Auto one day"
+							/>
+							<Button bsStyle="info" simple type="button" bsSize="small" onClick={e => this.login()}>
+								Login
+							</Button>
+						</div>
+					</form>
 				</div>
-			);
-    }
+			</div>
+		);
+	}
 }
 
 export default LoginComp;
