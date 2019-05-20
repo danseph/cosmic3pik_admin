5d0b6ab85c7a3daa950c351e494dfa3516da5f7f
hoi <hoi@neuralbc.com>
2019-05-20 14:07:37 +0900

    출금 진행중 테스트

--- src/views/Withdraw/WithdrawLog.jsx
+++ src/views/Withdraw/WithdrawLog.jsx
@@ -7,6 +7,7 @@ import { NavLink } from "react-router-dom";
 import axios from 'axios';
 import cp from '../../cp';
 import moment from 'moment';
+import Button from "components/CustomButton/CustomButton.jsx";
 
 class VoteList extends Component {
     constructor(props) {
@@ -46,31 +47,61 @@ class VoteList extends Component {
 		var tableTh = [];
 		tableTh.push(
 			<tr key="1">
-				<th style={Object.assign({}, style.Config.w20, style.Config.wordCenter, style.Config.wordBlod)} >유저id (추후 닉네임추가)</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >신청 ai수량</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >스왑코인</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >변환된 수량</th>
+				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >유저id (추후 닉네임추가)</th>
+				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >신청 ai수량</th>
+				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >스왑코인</th>
+				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >변환된 수량</th>
 				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >지갑주소</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >진행상태</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >신청날짜</th>
-				<th style={Object.assign({}, style.Config.w10, style.Config.wordCenter, style.Config.wordBlod)} >txid</th>
+				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >진행상태</th>
+				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >신청날짜</th>
+				<th style={Object.assign({}, style.Config.w15, style.Config.wordCenter, style.Config.wordBlod)} >txid</th>
+				<th style={Object.assign({}, style.Config.w15, style.Config.wordCenter, style.Config.wordBlod)} >기능</th>
 			</tr>
 
 		)
 
 		for (var item of this.state.data) {
             var crDate = moment(item.cr_date).format('YYYY-MM-DD HH:mm');
-
+            var userStatus = {'R' : '준비중' , 'I' : "진행중" , 'C' : '진행완'};
+            if(item.status )
 			tableTd.push(
 				<tr style={style.Config.pointer} key={item._id} >
-					<td style={Object.assign({}, style.Config.w20, style.Config.wordCenter)}>{item.user_id}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.ai_amount}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.coin_type}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.coin_amount}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.wallet}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.status}</td>
-					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.total_amount}</td>
+					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{item.user_id}</td>
+					<td style={Object.assign({}, style.Config.w5, style.Config.wordCenter)}>{item.ai_amount}</td>
+					<td style={Object.assign({}, style.Config.w5, style.Config.wordCenter)}>{item.coin_type}</td>
+					<td style={Object.assign({}, style.Config.w5, style.Config.wordCenter)}>{item.coin_amount}</td>
+					<td style={Object.assign({}, style.Config.w10, style.Config.wordLeft)}>{item.wallet}</td>
+					<td style={Object.assign({}, style.Config.w5, style.Config.wordCenter)}>{userStatus[item.status]}</td>
 					<td style={Object.assign({}, style.Config.w10, style.Config.wordCenter)}>{crDate}</td>
+					<td style={Object.assign({}, style.Config.w35, style.Config.wordCenter)}>{item.txid}</td>
+    				<th style={Object.assign({}, style.Config.w5, style.Config.wordCenter, style.Config.wordBlod)} >
+                        <Button
+                            style={{
+                                width: '5em',
+                                padding: '.5em .1em',
+                                margin: '0 .2em .2em 0'
+                            }}
+                            bsStyle="info"
+                            pullLeft
+                            fill
+                            onClick={() => { alert('준비중입니다.') }}
+                            type="button">
+                            Edit
+                        </Button>
+
+                        <Button
+                            style={{
+                                width: '5em',
+                                padding: '.5em .1em'
+                            }}
+                            bsStyle="danger"
+                            pullLeft
+                            fill
+                            onClick={() => { alert('준비중입니다.') }}
+                            type="button">
+                            Delete
+                        </Button>
+                    </th>
 				</tr>);
 
 		}
